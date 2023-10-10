cd853023daae5249835c76c8dccf8a30e95fd920
sugarlic <126883774+sugarlic@users.noreply.github.com>
2023-10-10 17:20:23 +0300

    Revert "Fix file reopen"

--- src/Model/Model.h
+++ src/Model/Model.h
@@ -1,14 +1,12 @@
 #ifndef MODEL_H_
 #define MODEL_H_
 
-#include <qimage.h>
-
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
 
-#include "EasyBMP.h"
+#include "../../Libs/EasyBMP/EasyBMP.h"
 
 namespace s21 {
 class Model {
@@ -16,33 +14,30 @@ class Model {
   Model(){};
   ~Model(){};
   void ReadImg(const std::string &img_name);
-  QImage WriteImg();
-  void ChannelSelection(int red, int green, int blue);
+  void WriteImg(const std::string &img_name);
+  void ChannelSelection(char RGB);
   void AverageConversion();
   void ConversionByBrightness();
   void ConversionByDesaturation();  // ????
   void Negative();
   void Convolution(const std::string &convolution_name);
   void SobelFilterCombin();
-  void ArbitraryMatrixMode(const std::vector<std::vector<double>> &matrix);
-  void Restart();
 
  private:
   std::vector<std::vector<EasyBMP::RGBApixel>> img_matrix_;
   std::vector<std::vector<EasyBMP::RGBApixel>> filtered_matrix_;
   const std::map<std::string, std::vector<std::vector<double>>> kernel_map_{
-      {"Original image", {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}},
-      {"Embos", {{-2, -1, 0}, {-1, 1, 1}, {0, 1, 2}}},
-      {"Sharpen", {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}}},
-      {"Box blur", {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}},  // ????
-      {"Gaussian blur",
+      {"original_image", {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}},
+      {"embos", {{-2, -1, 0}, {-1, 1, 1}, {0, 1, 2}}},
+      {"sharpen", {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}}},
+      {"box_blur", {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}},  // ????
+      {"gaussian_blur",
        {{1 / 2.5, 2 / 2.5, 1 / 2.5},
         {2 / 2.5, 4 / 2.5, 2 / 2.5},
         {1 / 2.5, 2 / 2.5, 1 / 2.5}}},  // ????
-      {"Laplacian filter", {{-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}}},
-      {"Sobel filter left", {{1, 0, -1}, {2, 0, -2}, {1, 0, -1}}},
-      {"Sobel filter right", {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}}};
-  QImage CreateQimage(BMP image);
+      {"laplacian_filter", {{-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}}},
+      {"sobel_filter_left", {{1, 0, -1}, {2, 0, -2}, {1, 0, -1}}},
+      {"sobel_filter_right", {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}}};
 };
 }  // namespace s21
 
